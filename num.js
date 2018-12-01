6dc17eccf5a76fda53ea806eaa016911d976ff5f
Betu <bkay210@gmail.com>
2018-11-30 23:55:50 -0500

    add numbers

---
+++ num.js
@@ -0,0 +1,4 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+ctx.font = "30px Arial";
+ctx.strokeText("Hello World",10,50);
--- sketch.js
+++ sketch.js
@@ -1,3 +1,5 @@
+
+
 function setup(){
   createCanvas(400, 400);
   angleMode(DEGREES);
@@ -6,25 +8,25 @@ function setup(){
 function draw(){
   background(0);
 
-  let hr = hour();
+  let hr = hour() % 12;
   let mn = minute();
   let sc = second();
 
   strokeWeight(8);
   stroke(255, 100, 150);
   noFill();
-  let end1 = map(sc, 0, 60, 0, 360);
-  arc(200, 200, 300, 300, -90, end1);
+  let secHand = map(sc, 0, 60, 0, 360);
+  arc(200, 200, 300, 300, -90, secHand);
 
   stroke(150, 100, 255);
-  let end2 = map(mn, 0, 60, 0, 360);
-  arc(200, 200, 280, 280, -90, end2);
+  let minHand = map(mn, 0, 60, 0, 360);
+  arc(200, 200, 280, 280, -90, minHand);
 
   stroke(150, 255, 255);
-  let end3 = map(hr%12, 0, 12, 0, 360);
-  arc(200, 200, 260, 260, -90, end3);
+  let hourHand = map(hr%12, 0, 12, 0, 360);
+  arc(200, 200, 260, 260, -90, hourHand);
 
-//  fill(255);
-//  noStroke();
-//  text(hr + ':' + mn + ':' + sc, 10, 200);
+  fill(255);
+  noStroke();
+  text(hr + ':' + mn + ':' + sc, width * .45, 200);
 }
